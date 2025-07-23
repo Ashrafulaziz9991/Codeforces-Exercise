55b7c441caaa177fdf7908015e32a67d045b0a2c
Ashrafulaziz9991 <ashrafulaziz9991@email.com>
2025-07-23 22:10:10 +0600

    Even odds

--- Even Odds.cpp
+++ Even Odds.cpp
@@ -6,16 +6,44 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    long long int n, position, idx = 0;
-    cin >> n >> position;
-    long long int vec[n];
-    for (int i = 1; i <= n; i += 2 && int j = 2 ; j <= n; j+=2){
-        vec[idx++] = i;
-        vec[idx++] = j;
-    }
-    for (int i = 2; i <= n; i += 2)
-        vec[idx++] = i;
+    
 
-    cout << vec[position - 1];
+    long long int n, position;
+    cin >> n >> position;
+
+    long long int oddCount = (n + 1) / 2;
+
+    if (position <= oddCount)
+    {
+        cout << 2 * position - 1 << endl;
+    }
+    else
+    {
+        long long int evenPos = position - oddCount;
+        cout << 2 * evenPos << endl;
+    }
+
+
+    // long long int n, position, idx = 0;
+    // cin >> n >> position;
+    // vector<long long int>vec;
+    // for (long long int i = 1; i <= n; i += 2){
+    //     idx++;
+    //     if (position == idx)
+    //     {
+    //         cout<< i <<endl;
+    //         break;
+    //     }
+    // }
+
+    // for (long long int i = 2; i <= n; i += 2){
+    //     idx++;
+    //     if (position == idx)
+    //     {
+    //         cout<< i <<endl;
+    //         break;
+    //     }
+    // }
+    
     return 0;
 }
