7f67dff9fe86be9375ffe42427d1ea5561a3fec6
Ashrafulaziz9991 <ashrafulaziz9991@email.com>
2025-07-24 12:31:31 +0600

    Lucky Division

--- Lucky Division.cpp
+++ Lucky Division.cpp
@@ -1,40 +1,62 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// set<int> Lucky;
+// bool is_lucky(int &num)
+// {
+//     while (num > 0)
+//     {
+//         int temp = num % 10;
+//         Lucky.insert(temp);
+//         num = num / 10;
+//     }
+//     for (auto &&i : Lucky)
+//     {
+//         if (Lucky.size() == 2 && i == 4 && i == 7)
+//             return true;
+//         else if (Lucky.size() == 1 && i == 4 || i == 7)
+//             return true;
+//     }
+    
+//     return false;
+// }
+
+// int main()
+// {
+//     ios_base::sync_with_stdio(0);
+//     cin.tie(0);
+
+//     int n;
+//     cin >> n;
+
+//     if (n % 4 == 0 || n % 7 == 0)
+//         printf("Yes\n");
+//     else if (is_lucky(n))
+//         printf("YES");
+//     else
+//         printf("NO");
+
+//     return 0;
+// }
+
+
 #include <bits/stdc++.h>
 using namespace std;
-
-set<int> Lucky;
-bool is_lucky(int &num)
-{
-    while (num > 0)
-    {
-        int temp = num % 10;
-        Lucky.insert(temp);
-        num = num / 10;
-    }
-    for (auto &&i : Lucky)
-    {
-        if (Lucky.size() == 2 && i == 4 && i == 7)
-            return true;
-        else if (Lucky.size() == 1 && i == 4 || i == 7)
-            return true;
-    }
-    
-    return false;
-}
-
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
+    
+    vector<int>lst = {4, 7, 44, 47, 74, 77, 444, 447, 474, 477, 744, 747, 774, 777};
 
-    int n;
-    cin >> n;
+    int n, count = 0;
+    cin>>n;
 
-    if (n % 4 == 0 || n % 7 == 0)
-        printf("Yes\n");
-    else if (is_lucky(n))
-        printf("YES");
-    else
-        printf("NO");
-
+    for (int i = 0; i < lst.size(); i++)
+    {
+        if (n % lst[i] == 0)
+            count++;
+    }
+    count > 0 ? cout<< "YES" : cout<<"NO"<<endl;
     return 0;
 }
