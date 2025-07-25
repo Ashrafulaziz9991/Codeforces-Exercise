303cb1aa319aef726281cee422fdbaf0da5b8ba4
Ashrafulaziz9991 <ashrafulaziz9991@gmail.com>
2025-07-25 20:00:47 +0600

    Isomia Cure

--- Insomnia cure.cpp
+++ Insomnia cure.cpp
@@ -1,31 +1,31 @@
+/*
+ * completed
+ */
 #include <bits/stdc++.h>
 using namespace std;
-
-/**
- * not completed
- */
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    
+
     int k, l, m, n, d;
-    cin>>k>>l>>m>>n>>d;
+    cin >> k >> l >> m >> n >> d;
 
     int count = 0;
 
-    for (int i = 1; i <=d ; i++)
+    for (int i = 1; i <= d; i++)
     {
         if (i % k == 0)
-            count+=k;
-        if( i % l == 0)
-            count+=l;
-        if( i % m == 0)
-            count+=m;
-        if( i % n == 0)
-            count+=n;
+            count++;
+        else if (i % l == 0)
+            count++;
+        else if (i % m == 0)
+            count++;
+        else if (i % n == 0)
+            count++;
+        else if (i % d == 0)
+            count++;
     }
-
-    cout<< count;
+    cout << count;
     return 0;
 }
