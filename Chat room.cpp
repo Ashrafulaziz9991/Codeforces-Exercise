106e55eb5bc25d1ddc7556119fd027dbd2091235
Ashrafulaziz9991 <ashrafulaziz9991@gmail.com>
2025-09-02 12:53:14 +0600

    string problem

--- Chat room.cpp
+++ Chat room.cpp
@@ -1,63 +1,65 @@
 // problem link : https://codeforces.com/problemset/problem/58/A
-/*
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
     ios_base::sync_with_stdio(0);
-    // cin.tie(0);
-    // int n, m;
-    // cin >> n >> m;
+    cin.tie(0);
+
     string s;
     cin >> s;
-    // cin.ignore();
-    // getline(cin, s);
-
-    // if(s == "hello" || s == "ahhellllloou")
-    //     cout << "YES";
-    // else
-    //     cout<<"NO";
-
-
-    // cout << s;
 
     // O(n^5)
 
-    for (int i = 0; i < s.size(); i++)
+    // {
+    //     if (s[i] == 'h')
+    //     {
+    //         for (int j = i + 1; j < s.size(); j++)
+    //         {
+    //             if (s[j] == 'e')
+    //             {
+    //                 for (int k = j + 1; k < s.size(); k++)
+    //                 {
+    //                     if (s[k] == 'l')
+    //                     {
+    //                         for (int l = k + 1; l < s.size(); l++)
+    //                         {
+    //                             if (s[l] == 'l')
+    //                             {
+    //                                 for (int m = l + 1; m < s.size(); m++)
+    //                                 {
+    //                                     if (s[m] == 'o')
+    //                                     {
+    //                                         cout << "YES";
+    //                                         return 0;
+    //                                     }
+    //                                 }
+    //                             }
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+    string str = "hello";
+    
+    int idx = 0;
+
+    for (auto i : s)
     {
-        if (s[i] == 'h')
-        {
-            for (int j = i + 1; j < s.size(); j++)
-            {
-                if (s[j] == 'e')
-                {
-                    for (int k = j + 1; k < s.size(); k++)
-                    {
-                        if (s[k] == 'l')
-                        {
-                            for (int l = k + 1; l < s.size(); l++)
-                            {
-                                if (s[l] == 'l')
-                                {
-                                    for (int m = l + 1; m < s.size(); m++)
-                                    {
-                                        if (s[m] == 'o')
-                                        {
-                                            cout << "YES";
-                                            return 0;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        if(i == str[idx])
+            idx++;
+        if(idx == str.size())
+            break;
     }
-    cout<<"NO";
+    
+    if(idx == str.size())
+        cout << "YES";
+    else
+        cout << "NO";
     return 0;
 }
 
-*/
