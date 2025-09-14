91198a1643a6aed4718c7cc3b764845593c4b8f8
Ashrafulaziz9991 <ashrafulaziz9991@gmail.com>
2025-09-14 20:25:38 +0600

    Initial commit

---
+++ Sublime Sequence.cpp
@@ -0,0 +1,22 @@
+// problem link https://codeforces.com/problemset/problem/2148/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, n;
+        cin >> x >> n;
+        if (n % 2 == 0)
+            cout << 0 << "\n";
+        else
+            cout << x << "\n";
+    }
+    return 0;
+}
