3a8dd48983397837a3111492af982d2b5220415d
Ashrafulaziz9991 <ashrafulaziz9991@gmail.com>
2026-01-31 21:49:09 +0600

    done after trying a few stpeps later

--- Even Array.cpp
+++ Even Array.cpp
@@ -1,25 +1,54 @@
-#include<bits/stdc++.h>
-//we've to learn first greedy algorothm to solve this task
-//it's will be hanging on until we didn't learn that algorithm
+// problem link : https://codeforces.com/problemset/problem/1367/B
+
+#include <iostream>
+#include <vector>
+
 using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int>v(n);
+    for(int i = 0; i < n; i++)
+        cin >> v[i];
+    
+    int even_count = 0, odd_count = 0;
+    int even_idx = 0, odd_idx = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        if(v[i] % 2 == 0) even_count++;
+        else odd_count++;
+
+        if(i % 2 == 0) even_idx++;
+        else odd_idx++;
+    }
+
+    if(even_count != even_idx || odd_count != odd_idx){
+        cout << -1 << endl;
+        return;
+    }
+    int mismatches = 0;
+    for(int i = 0; i < n; i++)
+    {
+        if(i % 2 !=  v[i] % 2) mismatches++;
+    }
+    cout << mismatches / 2 << endl;
+    return;
+}
 
 int main()
 {
-    int n, flag=0;cin>>n;
-    vector<int>arr(n);
-    for(int i=0;i<n;i++)
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while (t--)
     {
-        cin>>arr[i];
-        if(arr[i]==i)
-        {
-            flag=1;
-            break;
-        }
-    }
-    for(int i=0;i<n;i++)
-    {
-        cout<<arr[i]<<"\n";
+        solve();
     }
     return 0;
-
 }
