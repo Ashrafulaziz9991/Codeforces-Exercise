0dc97b131ccdefa4c175dcc4e6ea3b43f74a16f0
Ashrafulaziz9991 <ashrafulaziz9991@gmail.com>
2026-03-05 00:11:04 +0600

    added optimized version

--- spy detected.cpp
+++ spy detected.cpp
@@ -1,3 +1,6 @@
+// problem link : https://codeforces.com/problemset/problem/1512/A
+
+/**
 #include <bits/stdc++.h>
 using namespace std;
 int main()
@@ -31,5 +34,51 @@ int main()
         }
         cout << index << endl;
     }
+    return 0;
+}
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <climits>
+#include <cstring>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+    int spy;
+    unordered_map<int, int> ump;
+    for (int i : v) ump[i]++;
+    for (int i = 0; i < v.size(); i++)
+        if (ump[v[i]] == 1)
+            spy = i + 1;
+
+    cout << spy << endl;;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
     return 0;
 }
